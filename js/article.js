320a0a5d021541f30e2ca232c7502fe965ae32d5
Nicolas Tuller <tullern85@hotmail.fr>
2021-09-02 17:23:48 +0200

    page article

--- js/article.js
+++ js/article.js
@@ -1,31 +1,99 @@
-(async function() {
-    const articleId = getArticleId()
-    const article = await getArticle(articleId)
-    console.log(article)
-    displayArticle(article)
-})()
+fetch ("http://localhost:3000/api/teddies/" + teddieId()) 
+    .then (response=>response.json())
+    .then(teddie=>{
+        viewTeddie(teddie);
+})
+    .catch (function (err) {
+        window.alert('Le serveur n\'est pas disponible, essayez ultérieurement.')
+  });
 
-function getArticleId() {
-    return new URL(location.href).searchParams.get("id")
+  function teddieId() { // fonction qui recupère l 'ID du teddie
+    return location.search.split('=')[1];
+  }
+
+  function getSelectedColors() { // fonction qui permet de selectionner la couleur 
+    return colors.value;
+  }
+
+  // fonction qui affiche la fiche produit
+function viewTeddie(teddie) {
+
+    let newDiv = document.createElement('div');
+  newDiv.className = 'card';
+
+  teddieChoix.appendChild(newDiv);
+
+  let img = document.createElement('img');
+  img.className = 'card-img-top';
+  img.src = teddie.imageUrl;
+  img.alt = teddie.name;
+
+  newDiv.appendChild(img);
+
+  let div1 = document.createElement('div');
+  div1.className = 'card-body';
+
+  let h1 = document.createElement('h1');
+  h1.className = 'card-title text-center';
+  h1.innerHTML = teddie.name;
+
+  div1.appendChild(h1);
+
+  let p = document.createElement('p');
+  p.className = 'card-text text-center';
+  p.innerHTML = teddie.description;
+
+  div1.appendChild(p);
+
+  let select = document.createElement('select');
+  select.className = 'custom-select';
+  select.id = 'colors';
+
+  div1.appendChild(select);
+
+  // boucle pour afficher les différentes couleurs des teddies
+  for (let i = 0; i < teddie.colors.length; i++) {
+    let option = document.createElement('option');
+    option.text = teddie.colors[i];
+    select.appendChild(option);
+
 }
 
-function getArticle(articleId) {
-    return fetch('http://localhost:3000/api/teddies/?id=')
-        .then(function(httpBodyResponse) {
-            return httpBodyResponse.json()
-        })
-        .then(function(articles) {
-           return articles
-        })
-        .catch(function(error) {
-            window.alert('Le serveur n\'est pas disponible, essayez ultérieurement.')
-        })
-}
+    let string = document.createElement('string');
+    string.className = 'card-price';
+    string.innerHTML = teddie.price/100 + " €";
 
-function displayArticle(article) {
+    div1.appendChild(string);
 
-    document.getElementById("name").textContent = article.name
-    document.getElementById("imageUrl").src = article.imageUrl
-    document.getElementById("description").textContent = article.description
-    document.getElementById("price").textContent = article.price / 100 + " €"
-}
+newDiv.appendChild(div1);
+
+  let button = document.createElement('button');
+  button.setAttribute = 'type', 'submit';
+  button.id = 'btnAddCart'
+  button.className = 'btn';
+  button.innerHTML = 'Ajouter au panier';
+
+  newDiv.appendChild(button);
+
+  button.addEventListener('click', addProduct) 
+  
+  function addProduct(){ // fonction pour ajouter le produit au localstorage
+
+    let products = []; 
+    if (getSelectedColors()) {
+      if(localStorage.getItem('products')) {
+        products = JSON.parse(localStorage.getItem('products')); 
+      }
+
+      products.push({_id : teddie._id, image : teddie.imageUrl, name : teddie.name, colors : colors.value, price : teddie.price/100 });
+      localStorage.setItem('products', JSON.stringify(products));
+      alert('Ajouté au panier, Merci !');
+      window.location.reload();
+      
+    } 
+  };
+
+};
+
+// fonction qui affiche le nombre d'article dans le panier
+//displayQty();
